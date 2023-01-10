10ff34d74306156bc5d8da12ee92a79382c30a18
praggyaverma <119279862+praggyaverma@users.noreply.github.com>
2023-01-10 22:22:47 +0530

    Submitting Q2 of Assignment 1

---
+++ a1_q2.py
@@ -0,0 +1,11 @@
+m=int(input())
+if m>90:
+    print("excellent")
+elif m>80 and m<=90:
+    print("good")
+elif m>70 and m<=80:
+    print("fair")
+elif m>60 and m<=70:
+    print("meets expectations")
+elif m<=60:
+    print("below par")
